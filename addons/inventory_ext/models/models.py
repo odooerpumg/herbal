16f0f369e9c99ff4fe9d5bb929271abbe86df58f
thuyahtut <mgthuyahtut@gmail.com>
2021-08-12 16:55:29 +0630

    customer data customization and POS clientlist screen customization

--- addons/inventory_ext/models/models.py
+++ addons/inventory_ext/models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from typing import Text
 from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -21,3 +23,35 @@ class ProductTemplate(models.Model):
     def _onchange_list_price(self):
         self.umgian_price = self.list_price if self.list_price and self.umgian_price == 1.0 else self.umgian_price
         self.umgian_family_price = self.list_price if self.list_price and self.umgian_family_price == 1.0 else self.umgian_family_price
+
+
+class ResPartner(models.Model):
+    """Partner with birth date in date format."""
+
+    _inherit = "res.partner"
+
+    birthdate_date = fields.Date("Birthdate")
+    age = fields.Integer(string="Age", readonly=True, compute="_compute_age")    
+
+    # REFEREE
+    referee_name = fields.Char(string='Referee Name')
+    referee_number = fields.Char(string='Referee Number')
+
+    # SECOND CONTACT
+    sec_contact_name = fields.Char(string='Second Contact Name')
+    sec_contact_number = fields.Char(string='Second Contact Nunmber')
+
+    #CUSTOMER TYPE(UMGian/UMGina Family Member)
+    customer_type = fields.Selection([('umgian', 'UMGian'),('umgian_family_member', 'UMGina Family Member')], string='Customer Type')
+
+    # TEXT
+    medical_history = fields.Text(string='Medical History')
+    remark = fields.Text(string='Remark')
+
+    @api.depends("birthdate_date")
+    def _compute_age(self):
+        for record in self:
+            age = 0
+            if record.birthdate_date:
+                age = relativedelta(fields.Date.today(), record.birthdate_date).years
+            record.age = age 
