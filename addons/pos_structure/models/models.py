e9fdc608c584f477f0c2bea03ff543ac975ff905
odooerpumg <odooerpumg@gmail.com>
2021-10-01 17:00:56 +0630

    uypdate

--- addons/pos_structure/models/models.py
+++ addons/pos_structure/models/models.py
@@ -10,3 +10,11 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     cashier_ids = fields.One2many('res.users', 'pos_session_id', string='Cashiers')
+    
+    cashier_count = fields.Integer(string="Assigned cashier", compute="_compute_cashiers_list")
+
+    @api.depends('cashier_ids')
+    def _compute_cashiers_list(self):
+        for record in self:
+            record.cashier_count = len(record.cashier_ids)
+            print("record.cashier_list_str",record.cashier_count)
