1cbd0149baf6357c771382d008e3637cb99d5664
odooerpumg <odooerpumg@gmail.com>
2021-12-22 15:31:39 +0630

    UPDATE: pos order import action and sample template format

--- addons/import_pos_orders/models/models.py
+++ addons/import_pos_orders/models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 import xlrd
 import base64
 class import_pos_orders(models.TransientModel):
@@ -8,6 +8,13 @@ class import_pos_orders(models.TransientModel):
 
     name = fields.Char()
     session_id = fields.Many2one('pos.session', string='Session')
+    config_id = fields.Many2one('pos.config', related='session_id.config_id', string='POS config')
+    payment_method_id = fields.Many2one('pos.payment.method', string='Payment Method')
+    
+    # customer
+    customer_included = fields.Boolean("Customer Included In Template." , default=True)
+    partner_id = fields.Many2one('res.partner', string='Customer') 
+
     orders_lists_file = fields.Binary(string='File' )
 
     def action_import(self):
@@ -17,6 +24,7 @@ class import_pos_orders(models.TransientModel):
             'line_price_unit', 'discount'
         ]
         model = self.env['pos.order']
+        payment_model = self.env['pos.make.payment']
     # 
         # data = {
         #     'user_id': 2,
@@ -62,46 +70,79 @@ class import_pos_orders(models.TransientModel):
         lines = []
         for rowx, row in enumerate(map(sheet.row, range(sheet.nrows)), 1):
             if rowx > 1:
-                if row[0].value == 'order':
+                # ENDING FLAG
+                if row[0].value == 'end':
+                    if order and lines:
+                        order['lines'] = lines
+                        data.append(order)    
+                                    
+                # ORDER
+                elif row[0].value == 'order':
                     if order and lines:
                         order['lines'] = lines
                         data.append(order)
                         order = {}
                         lines = []
-                    order['user_id'] = int(row[1].value)
-                    order['session_id'] = int(row[2].value)
-                    order['pos_reference'] = row[3].value
-                    order['partner_id'] = row[4].value
-                    order['fiscal_position_id'] = row[5].value
+                    order['user_id'] = self.env.user.id
+                    order['session_id'] = self.session_id.id
+                    order['pos_reference'] = row[1].value
+
+                    if self.customer_included:
+                        order['partner_id'] = row[2].value
+                    else:
+                        order['partner_id'] = self.partner_id.id
+
+                    order['amount_total'] = row[3].value
+                    order['amount_paid'] = row[3].value
+                    order['date_order'] = row[4].value
+
+                    order['fiscal_position_id'] = False
                     order['amount_tax'] = 0
-                    order['amount_total'] = row[10].value
-                    order['amount_paid'] = row[10].value
                     order['amount_return'] = 0
-
-                if row[0].value == 'end':
-                    if order and lines:
-                        order['lines'] = lines
-                        data.append(order)
-
-                if row[0].value == 'line' and order:
+                
+                # ORDER LINE ITEMS
+                elif row[0].value == 'line' and order:
                     line = [
                         0, 0,
                         {
-                            'product_id': int(row[6].value),
-                            'qty': int(row[7].value),
-                            'price_unit': float(row[8].value),
-                            'price_subtotal': int(row[7].value) * float(row[8].value),
-                            'price_subtotal_incl': int(row[7].value) * float(row[8].value),
-                            'discount': 0.0, 
+                            'product_id': int(row[5].value),
+                            
+                            # PRICE
+                            'qty': int(row[6].value),
+                            'price_unit': float(row[7].value),
+                            'price_subtotal': int(row[6].value) * float(row[7].value),
+                            'price_subtotal_incl': int(row[6].value) * float(row[7].value),
+
+                            'discount': float(row[8].value), 
                             'tax_ids': [[6, False, []]],
                             'pack_lot_ids': [],
-                            # 'name': 'Backlogs/0046'
+                            'name': 'Backlogs/Session-%s' % self.session_id.name
                         }
                     ]
                     lines.append(line)
+        _ids = []
         for o in data:
-            model.create(o)
-        return True
+            current_order = model.create(o)
+            _ids.append(current_order.id)
+            # if current_order:
+            #     current_order.add_payment({
+            #                     'pos_order_id': current_order.id,
+            #                     'amount': current_order._get_rounded_amount(init_data['amount']),
+            #                     'name': init_data['payment_name'],
+            #                     'payment_method_id': self.payment_method.id,
+            #                 })  
+        
+        # Domain to filterout only current created order
+        domain = [('id', 'in', _ids)]
+        action = {
+            'name': _('Orders'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'pos.order',
+            'view_type': 'list',
+            'view_mode': 'list,form',
+            'domain': domain,
+        }
+        return action
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
